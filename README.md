289d09df5c65c7e8a4c601dd0165ae9d6dc97a74
15MiConroy <michaelshawnconroy@outlook.com>
2024-02-20 01:37:36 -0500

    Added post request data
    Added post request data

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Potential Approaches:
 1) Do full search on properties that have more than 1 bathroom
 2) Use selenium to loop through all results and collect parcel number and link to details page
 3) 
+
+.....
+
+Github Copilot came up with a better solultion!!! I can open the search page through selenium, get the necessary headers and body tags!
+Then I can query on 5000 records. I tested all of this using postman and it worked! Max amount of records I can get out is currently 5k. So I will need to find a way to query the rest in a systematic way.
+
+I may or may not have gotten the request to work via python. Need to revisit once I have had some sleep :P
--- query_data_selenium.py
+++ query_data_selenium.py
@@ -15,6 +15,31 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 # Find the VIEWSTATE and EVENTVALIDATION values
 viewstate = soup.find(id="__VIEWSTATE")['value']
 eventvalidation = soup.find(id="__EVENTVALIDATION")['value']
+viewstategenerator = soup.find(id="__VIEWSTATEGENERATOR")['value']
+sortBy = soup.find(id="SortBy")['value']
+sortDir = soup.find(id="SortDir")['value']
+pageSize = soup.find(id="PageSize")['value']
+hdCriteria = "bedrooms|1~99"
+hdSearchType = soup.find(id="hdSearchType")['value']
+hdCriteriaType = "AdvSearch"
+hdLastState = soup.find(id="hdLastState")['value']
+hdSelectedQuery = soup.find(id="hdSelectedQuery")['value']
+hdCriterias = soup.find(id="hdCriterias")['value']
+hdSelectAllChecked = "false"
+ctl01dlGroups = "4"
+inpDistinct = "on"
+ctl01_cal1_dateInput_ClientState = soup.find(id="ctl01_cal1_dateInput_ClientState")['value']
+ctl01_cal1_calendar_SD  = soup.find(id="ctl01_cal1_calendar_SD")['value']
+ctl01_cal1_calendar_AD = soup.find(id="ctl01_cal1_calendar_AD")['value']
+ctl01_cal1_ClientState = soup.find(id="ctl01_cal1_ClientState")['value']
+ctl01_cal2_dateInput_ClientState = soup.find(id="ctl01_cal2_dateInput_ClientState")['value']
+ctl01_cal2_calendar_SD = soup.find(id="ctl01_cal2_calendar_SD")['value']
+ctl01_cal2_calendar_AD = soup.find(id="ctl01_cal2_calendar_AD")['value']
+ctl01_cal2_ClientState = soup.find(id="ctl01_cal2_ClientState")['value']
+selSortBy = sortBy
+selSortDir = sortDir
+selPageSize = pageSize
+
 
 # Close the browser
 driver.quit()
@@ -27,8 +52,31 @@ headers = {
 # Define the data for the POST request
 data = {
     '__VIEWSTATE': viewstate,
+    '__VIEWSTATEGENERATOR': viewstategenerator,
     '__EVENTVALIDATION': eventvalidation,
-    # Add other necessary fields here
+    'SortBy': sortBy,
+    'SortDir': sortDir,
+    'PageSize': pageSize,
+    'hdCriteria': hdCriteria,
+    'hdSearchType': hdSearchType,
+    'hdCriteriaType': hdCriteriaType,
+    'hdLastState': hdLastState,
+    'hdSelectedQuery': hdSelectedQuery,
+    'hdCriterias': hdCriterias,
+    'hdSelectAllChecked': hdSelectAllChecked,
+    'ctl01$dlGroups': ctl01dlGroups,
+    'inpDistinct': inpDistinct,
+    'ctl01_cal1_dateInput_ClientState': ctl01_cal1_dateInput_ClientState,
+    'ctl01_cal1_calendar_SD': ctl01_cal1_calendar_SD,
+    'ctl01_cal1_calendar_AD': ctl01_cal1_calendar_AD,
+    'ctl01_cal1_ClientState': ctl01_cal1_ClientState,
+    'ctl01_cal2_dateInput_ClientState': ctl01_cal2_dateInput_ClientState,
+    'ctl01_cal2_calendar_SD': ctl01_cal2_calendar_SD,
+    'ctl01_cal2_calendar_AD': ctl01_cal2_calendar_AD,
+    'ctl01_cal2_ClientState': ctl01_cal2_ClientState,
+    'selSortBy': selSortBy,
+    'selSortDir': selSortDir,
+    'selPageSize': selPageSize,
 }
 
 # Send the POST request
@@ -36,3 +84,7 @@ response = requests.post('https://auditor.lakecountyohio.gov/search/advancedsear
 
 # Print the response
 print(response.text)
+
+#write the response to a file
+with open('response.txt', 'w') as f:
+    f.write(response.text)
