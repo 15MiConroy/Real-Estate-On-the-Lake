5cd06c98a04102d54d14512fe85f58fb1f1029fd
15MiConroy <michaelshawnconroy@outlook.com>
2024-03-06 20:14:06 -0500

    rentcast

--- rentcast/get_rental_data.py
+++ rentcast/get_rental_data.py
@@ -1,5 +1,7 @@
 import requests
 import os
+import datetime
+import json
 
 # Send get request tot rentcast API
 headers = {
@@ -7,16 +9,22 @@ headers = {
     'X-Api-Key': os.environ.get('rentcast_api_key'),
 }
 
-params = {
-    'limit': '500',
-    'status': 'active',
-    'state': 'OH',
-    }
-response = requests.get('https://api.rentcast.io/v1/listings/sale', headers=headers, params=params)
+# params = {
+#     'limit': '500',
+#     'status': 'active',
+#     'state': 'OH',
+#     'city': 'Willoughby',
+#     'city': 'Mentor',
+#     'city': 'Willoughby Hills',
+#     'city': 'Eastlake',
+#     }
+response = requests.get('https://api.rentcast.io/v1/listings/sale?limit=500&status=active&state=OH&city=Willoughby&city=Mentor', headers=headers)
 
 if response.status_code == 200:
     #export the response to a json file
-    with open('rental_data.json', 'w') as f:
+    #set the filename to include the current date and time
+    filename = 'rental_data' + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.json'
+    with open(filename, 'w') as f:
         f.write(response.text)
 
     print('Data successfully retrieved and saved to rental_data.json')
